82480b66056aa145c217e5b1fd4598e7607b2505
Carlos-Zerda <czerda6@gmail.com>
2022-11-29 23:53:42 -0300

    preentrega1

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+let sino=prompt("Hola, Queres saber como se calcula el valor de tu cuota en un prestamo? si/no");
+if (sino === "si" || sino==="SI"|| sino==="Si"){
+
+    let nombreUsuario =prompt("Perfecto, decime cual es tu nombre para que comencemos a calcular");
+
+    alert("Ok "+nombreUsuario+" comencemos!!");
+
+    alert("Primero debemos ingresar algunos datos para poder realizar los calulos");
+
+    let monto = parseInt(prompt("sigamos, "+nombreUsuario+" ingrese el monto que desea solicitar: "));
+
+    let tiempo= parseInt(prompt(nombreUsuario+" ingrese la cantidad de tiempo (meses) en la que desea realizar el pago"))
+
+    let interes=parseInt(prompt("ok "+nombreUsuario+" ahora vamos a introducir un valor que es con el que los bancos y financieras trabajan que se llama TASA FIJA NOMINAL ANUAL, que se expresa en porcentaje (%)"));
+
+    alert("con estos datos ya podemos calcular el valor de las cuotas")
+    alert("la formular seria Monto = Cuota x (1-(1+TFNA)^(-TIEMPO) . Esto seria: cuota x (1-(1+"+interes+"%)^(-"+tiempo+"))/"+interes);
+
+    let exponente =tiempo*(-1)
+    let calculoParcial1=(1+(interes/1000))
+    let calculoParcial2=calculoParcial1**exponente
+    let calculoParcial3=((1-calculoParcial2))/(interes/1000)
+    let cuota = monto/calculoParcial3
+}
+else{alert ("ok, hasta la proxima   ")}
